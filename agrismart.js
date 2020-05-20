e3a22b38b997847f3d07e79909729d2e9f222d8f
ndriabecks <aandreani1998@gmail.com>
2020-05-20 20:51:08 +0200

    Completato scheletro di routing

--- agrismart.js
+++ agrismart.js
@@ -1,76 +1,147 @@
 #!/usr/bin/env node
 
-var request = require('request')
-var express     = require('express')
-var path        = require('path')
-var bodyparser  = require('body-parser')
+const path = require('path')
+const express = require('express')
+const request = require('request')
+const passport = require('passport')
+const bodyparser = require('body-parser')
+const session = require('express-session')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+
 require('dotenv').config()
 
-var app = express();
-app.use(bodyparser.urlencoded({extended: false}));
-
-const PORT      = 8888;
-
-// http status codes
-const HTTP_OK   = 200;
+const HTTP_OK = 200;
+const app = express();
+const PORT = process.env.PORT || 8888;
 
 // https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&exclude={}&appid=
-const OWM_URL_1     = 'https://api.openweathermap.org/data/2.5/onecall?'
-const OWM_URL_2     = '&appid=' + process.env.OWM_KEY;
+const OWM_URL_1 = 'https://api.openweathermap.org/data/2.5/onecall?'
+const OWM_URL_2 = '&appid=' + process.env.OWM_KEY;
 
 // https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/{lon,lat},14.25,0,45/600x600?access_token=
-const LL_URL_1      = 'https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/';
-const LL_URL_2      = ',10,0,45/600x600?access_token=' + process.env.LL_KEY;
+const LL_URL_1 = 'https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/';
+const LL_URL_2 = ',10,0,45/600x600?access_token=' + process.env.LL_KEY;
 
-function KelvinToCelcius(k){
-    return (parseFloat(k)-272.15).toPrecision(2);
-}
 
-// Impostazioni per il template engine
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+app.use(bodyparser.urlencoded({ extended: false }));
 
-// Seguendo il modello MVC qui abbiamo i controller
 
-app.get('/', function(req, res){
-    res.render('index.ejs');
+/***************************/
+/*   Add session support   */
+/***************************/
+app.use(session({
+	secret: process.env.SESSION_SECRET || 'default_session_secret',
+	resave: false,
+	saveUninitialized: false,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+	done(null, user);
 });
 
-app.post('/geolocation', function(req, res){
-    var latitude   = req.body.latitude;
-    var longitude = req.body.longitude; 
-    var url     = OWM_URL_1 + 'lat=' + latitude
-     + '&lon=' + longitude + '&exclude=' + 'minutely,hourly,current' + OWM_URL_2
+passport.deserializeUser((userDataFromCookie, done) => {
+	done(null, userDataFromCookie);
+});
 
-    request(url, function(error, response, body){
-        if(!error && response.statusCode == HTTP_OK){
 
-            var info    = JSON.parse(body).daily[0];
-            var main    = info.temp;
-            var weather = info.weather[0].description;
-            var map_url = LL_URL_1 + longitude + ',' + latitude + LL_URL_2;
+/***************************/
+/*        Functions        */
+/***************************/
 
-            res.render('dashboard.ejs', {
-                lat: latitude,
-                lon: longitude,
-                weather: weather,
-                temp: KelvinToCelcius(main.day),
-                feels_like: KelvinToCelcius(info.feels_like.day),
-                min: KelvinToCelcius(main.min),
-                max: KelvinToCelcius(main.max),
-                pressure: info.pressure,
-                humidity: info.humidity
-            });
+// Checks if a user is logged in
+const accessProtectionMiddleware = (req, res, next) => {
+	if (req.isAuthenticated()) {
+		next();
+	} else {
+		res.status(403).render('403forbidden.ejs');
+	}
+};
 
-        }
-        else{
-            console.log('ERROR');
-            res.end('404 NOT FOUND');
-        }
-    });
+// Set up passport strategy
+passport.use(new GoogleStrategy(
+	{
+		clientID: process.env.GOOGLE_OAUTH_TEST_APP_CLIENT_ID,
+		clientSecret: process.env.GOOGLE_OAUTH_TEST_APP_CLIENT_SECRET,
+		callbackURL: 'http://localhost:8888/auth/google/callback',
+		scope: ['email'],
+	}, (accessToken, refreshToken, profile, cb) => {
+		console.log('Our user authenticated with Google, and Google sent us back this profile info identifying the authenticated user:', profile);
+		return cb(null, profile);
+	},
+));
+
+function KelvinToCelcius(k) {
+	return (parseFloat(k) - 272.15).toPrecision(2);
+}
+
+/***************************/
+/*         Routing         */
+/***************************/
+
+app.get('/index', function (req, res) {
+	res.render('index.ejs');
+});
+
+// Create API endpoints
+
+// This is where users point their browsers in order to get logged in
+// This is also where Google sends back information to our app once a user authenticates with Google
+app.get('/auth/google/callback',
+	passport.authenticate('google', { failureRedirect: '/', session: true }),
+	(req, res) => {
+		console.log('wooo we authenticated, here is our user object:', req.user);
+		// res.json(req.user);
+		res.redirect('/index');
+	}
+);
+
+app.get('/', accessProtectionMiddleware, (req, res) => {
+	res.json({
+		message: 'You have accessed the protected endpoint!',
+		yourUserInfo: req.user,
+	});
+});
+
+app.post('/geolocation', function (req, res) {
+	var latitude = req.body.latitude;
+	var longitude = req.body.longitude;
+	var url = OWM_URL_1 + 'lat=' + latitude
+		+ '&lon=' + longitude + '&exclude=' + 'minutely,hourly,current' + OWM_URL_2
+
+	request(url, function (error, response, body) {
+		if (!error && response.statusCode == HTTP_OK) {
+
+			var info = JSON.parse(body).daily[0];
+			var main = info.temp;
+			var weather = info.weather[0].description;
+			var map_url = LL_URL_1 + longitude + ',' + latitude + LL_URL_2;
+
+			res.render('dashboard.ejs', {
+				lat: latitude,
+				lon: longitude,
+				weather: weather,
+				temp: KelvinToCelcius(main.day),
+				feels_like: KelvinToCelcius(info.feels_like.day),
+				min: KelvinToCelcius(main.min),
+				max: KelvinToCelcius(main.max),
+				pressure: info.pressure,
+				humidity: info.humidity
+			});
+
+		}
+		else {
+			console.log('ERROR');
+			res.end('404 NOT FOUND');
+		}
+	});
 });
 
 // Avvio del server
-var server = app.listen(PORT, function(){
-    console.log('[i] In ascolto su http://localhost:%s\n', PORT);
+var server = app.listen(PORT, function () {
+	console.log('[i] Agrismart su http://localhost:%s\n', PORT);
+	console.log(process.env.GOOGLE_OAUTH_TEST_APP_CLIENT_ID + "\n");
 });
--- login.js
+++ login.js
@@ -1,9 +1,9 @@
-const express = require('express');  
-const passport = require('passport');  
-const GoogleStrategy = require('passport-google-oauth20').Strategy;  
+const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const session = require('express-session');
 
-const app = express();  
+const app = express();
 const port = process.env.PORT || 8888;
 require('dotenv').config();
 
@@ -11,71 +11,71 @@ require('dotenv').config();
 app.use(express.static(__dirname + '/public'));
 
 // Add session support
-app.use(session({  
-  secret: process.env.SESSION_SECRET || 'default_session_secret',
-  resave: false,
-  saveUninitialized: false,
+app.use(session({
+	secret: process.env.SESSION_SECRET || 'default_session_secret',
+	resave: false,
+	saveUninitialized: false,
 }));
-app.use(passport.initialize());  
+app.use(passport.initialize());
 app.use(passport.session());
 
-passport.serializeUser((user, done) => {  
-  done(null, user);
+passport.serializeUser((user, done) => {
+	done(null, user);
 });
 
-passport.deserializeUser((userDataFromCookie, done) => {  
-  done(null, userDataFromCookie);
+passport.deserializeUser((userDataFromCookie, done) => {
+	done(null, userDataFromCookie);
 });
 
 // Checks if a user is logged in
-const accessProtectionMiddleware = (req, res, next) => {  
-  if (req.isAuthenticated()) {
-    next();
-  } else {
-    res.status(403).json({
-      message: 'must be logged in to continue',
-    });
-  }
+const accessProtectionMiddleware = (req, res, next) => {
+	if (req.isAuthenticated()) {
+		next();
+	} else {
+		res.status(403).json({
+			message: 'must be logged in to continue',
+		});
+	}
 };
 
 // Set up passport strategy
-passport.use(new GoogleStrategy(  
-  {
-    clientID: process.env.GOOGLE_OAUTH_TEST_APP_CLIENT_ID,
-    clientSecret: process.env.GOOGLE_OAUTH_TEST_APP_CLIENT_SECRET,
-    callbackURL: 'http://localhost:8888/auth/google/callback',
-    scope: ['email'],
-  },
-  (accessToken, refreshToken, profile, cb) => {
-    console.log('Our user authenticated with Google, and Google sent us back this profile info identifying the authenticated user:', profile);
-    return cb(null, profile);
-  },
+passport.use(new GoogleStrategy(
+	{
+		clientID: process.env.GOOGLE_OAUTH_TEST_APP_CLIENT_ID,
+		clientSecret: process.env.GOOGLE_OAUTH_TEST_APP_CLIENT_SECRET,
+		callbackURL: 'http://localhost:8888/auth/google/callback',
+		scope: ['email'],
+	},
+	(accessToken, refreshToken, profile, cb) => {
+		console.log('Our user authenticated with Google, and Google sent us back this profile info identifying the authenticated user:', profile);
+		return cb(null, profile);
+	},
 ));
 
 // Create API endpoints
 
 // This is where users point their browsers in order to get logged in
 // This is also where Google sends back information to our app once a user authenticates with Google
-app.get('/auth/google/callback',  
-  passport.authenticate('google', { failureRedirect: '/', session: true }),
-  (req, res) => {
-    console.log('wooo we authenticated, here is our user object:', req.user);
-    // res.json(req.user);
-    res.redirect('/pippo.html');
-  }
+app.get('/auth/google/callback',
+	passport.authenticate('google', { failureRedirect: '/', session: true }),
+	(req, res) => {
+		console.log('wooo we authenticated, here is our user object:', req.user);
+		// res.json(req.user);
+		res.redirect('/pippo.html');
+	}
 );
 
-app.get('/protected', accessProtectionMiddleware, (req, res) => {  
-  res.json({
-    message: 'You have accessed the protected endpoint!',
-    yourUserInfo: req.user,
-  });
+app.get('/protected', accessProtectionMiddleware, (req, res) => {
+	res.json({
+		message: 'You have accessed the protected endpoint!',
+		yourUserInfo: req.user,
+	});
 });
 
 // Start server
-const server = app.listen(port, function() {  
-  console.log('Server listening on port ' + port);
-  console.log(process.env.GOOGLE_OAUTH_TEST_APP_CLIENT_ID +"\n");
+const server = app.listen(port, function () {
+	console.log('Server listening on port ' + port);
+	console.log(process.env.GOOGLE_OAUTH_TEST_APP_CLIENT_ID + "\n");
 });
-   
-    
+
+
