802b93a83b79a4c1e246b6f509d47b25c14fe80f
ndriabecks <aandreani1998@gmail.com>
2020-05-27 15:33:10 +0200

    minor fixes

--- agrismart.js
+++ agrismart.js
@@ -14,6 +14,7 @@ var Promise = require('promise');
 //Per async
 var async = require("async");
 
+// Setup per accedere a firebase
 var admin = require("firebase-admin");
 var serviceAccount = "private/agrismart-c2656-firebase-adminsdk-1en4p-ba81ef792e.json";
 admin.initializeApp({
@@ -21,6 +22,7 @@ admin.initializeApp({
   databaseURL: "https://agrismart-c2656.firebaseio.com"
 });
 
+// Variabile di accesso al db
 let db = admin.firestore();
 
 
@@ -104,83 +106,6 @@ function KelvinToCelcius(k) {
 	return (parseFloat(k) - 272.15).toPrecision(2);
 }
 
-/***************************/
-/*         Routing         */
-/***************************/
-
-app.get('/index', accessProtectionMiddleware, function (req, res) {
-	res.render('index.ejs', {port: PORT, nomeutente: req.user.emails[0].value});
-});
-
-// Create API endpoints
-
-// This is where users point their browsers in order to get logged in
-// This is also where Google sends back information to our app once a user authenticates with Google
-app.get('/auth/google/callback',
-	passport.authenticate('google', { failureRedirect: '/', session: true }),
-	(req, res) => {
-		if(TEST) 
-			console.log('we authenticated, here is our user object:', req.user);
-
-		//mytest
-		createUser(req.user.emails[0].value);	
-		createCampo(req.user.emails[0].value, "campo1", 42, 12);
-		createSensore(req.user.emails[0].value, "campo1", "ID123883", "nomesensore");
-		createInnaffiamento(req.user.emails[0].value, "campo1", "ID123883", "1.3", "2019-02-20");
-			
-		res.redirect('/index');
-	}
-);
-
-app.get('/logout', function(req, res) {
-	req.logout();
-	res.redirect('/');
-});
-
-app.get('/', (req, res) => {
-	res.render('home.ejs', {auth: req.isAuthenticated()});
-});
-
-app.post('/geolocation', accessProtectionMiddleware, function (req, res) {
-	var latitude = req.body.latitude;
-	var longitude = req.body.longitude;
-	var url = OWM_URL_1 + 'lat=' + latitude
-		+ '&lon=' + longitude + '&exclude=' + 'minutely,hourly,current' + OWM_URL_2
-
-	request(url, function (error, response, body) {
-		if (!error && response.statusCode == HTTP_OK) {
-
-			var info = JSON.parse(body).daily[0];
-			var main = info.temp;
-			var weather = info.weather[0].description;
-			var map_url = LL_URL_1 + longitude + ',' + latitude + LL_URL_2;
-
-			res.render('dashboard.ejs', {
-				lat: latitude,
-				lon: longitude,
-				weather: weather,
-				temp: KelvinToCelcius(main.day),
-				feels_like: KelvinToCelcius(info.feels_like.day),
-				min: KelvinToCelcius(main.min),
-				max: KelvinToCelcius(main.max),
-				pressure: info.pressure,
-				humidity: info.humidity,
-				port: PORT
-			});
-
-		}
-	});
-});
-
-// Avvio del server
-var server = app.listen(PORT, function () {
-	if(TEST) console.log("[!] Output will be verbose, test mode on!");
-	console.log('[i] Agrismart su http://localhost:%s\n', PORT);
-	// console.log(process.env.GOOGLE_OAUTH_TEST_APP_CLIENT_ID + "\n");
-});
-
-
-
 /*************************************/
 /*         Database Firebase         */
 /*************************************/
@@ -188,10 +113,17 @@ var server = app.listen(PORT, function () {
 function createUser(email) { //creo l'utente
 	let instance = db.collection("users").doc(email);
 	instance.create({}).then(function() {
-			console.log("Utente aggiunto al database: "+ email);
-		}).catch((err) => {
-			console.log("Utente già registrato: "+ email);
-		});
+		console.log("Utente aggiunto al database: "+ email);
+	}).catch((err) => {
+		console.log("Utente già registrato: "+ email);
+	});
+
+	// campicounter serve per tenere traccia dell'indice da dare al campo
+	instance.set({
+		campicounter: 0
+	}).then(function() {
+		if(TEST) console.log("[+] campicounter set to 0");
+	})
 }
 
 function createCampo(email, name, lat, lon) { //creo il campo per l'utente
@@ -238,22 +170,22 @@ function createInnaffiamento(email, campo, sensore, umidita, data) { //creo inna
 }
 
 function promise2(doc){
-		if (!doc.exists) {
+	if (!doc.exists) {
 		console.log('No such document!');
 		const result =  new Promise((resolve)=>{
 			resolve("errore");
 		});
 		return result;
-		} else {
-			console.log('Document data:', doc.data());
+	} else {
+		console.log('Document data:', doc.data());
 			const result =  new Promise((resolve)=>{
 				resolve(doc.data());
 			});
 			return result;
 		}
-}
+	}
 
-function getSensorefromId(id){
+	function getSensorefromId(id){
 	return getSensorefromIdAux(id)
 	.then(promise2)
 	.catch((err) => console.log(err));
@@ -269,7 +201,7 @@ function getUltimiInnaffiamentiSensore(id){
 				snapshot.forEach(function(userSnapshot) {
 				  console.log(userSnapshot.data().data);
 				});
-			  });
+			});
 		}
 	});
 }
@@ -278,19 +210,25 @@ function getnomesensorefromId(id){
 	getSensorefromId(id)
 	.then((x) => {
 		if (x != "errore"){
-		//Qui devo cercare l'utente
-		let documento = db.collection("users").doc(x.email).collection("campi").doc(x.campo).collection("sensori").doc(id).get()
-		.then( (x) => {
+			//Qui devo cercare l'utente
+			let documento = db.collection("users").doc(x.email).collection("campi").doc(x.campo).collection("sensori").doc(id).get()
+			.then( (x) => {
 			console.log(x.data().name);
 			return x.data().name;
 		});		
-		}
+	}
 		else{
 			return "Errore";
 		}
 	})
 }*/
 
+function userExists(id) {
+	db.collection("users").doc(id).get().then((x) => {
+		if(x.exists) true;
+		else false;
+	});
+}
 
 function getSensoreFromId(id, callback){
 	db.collection("sensors").doc(id).get().then((x) => {
@@ -321,9 +259,91 @@ function getInnaffiamentiFromSensorID(id, callback) {
 	], callback);
 }
 
+/***************************/
+/*         Routing         */
+/***************************/
 app.get('/prova', (req, res) => {
 	getInnaffiamentiFromSensorID("ID123883", function (err, result) {
 		if(err != null) res.send(err);
 		else res.send(result);
 	});
+});
+
+app.get('/index', accessProtectionMiddleware, function (req, res) {
+	var umail = req.user.emails[0].value;
+	if(userExists(umail)) {
+		console.log("[!] Implementare get /dashboard");
+	} 
+	else {
+		createUser(umail);
+		res.render('index.ejs', {port: PORT, nomeutente: umail});
+	}
+});
+
+// Create API endpoints
+
+// This is where users point their browsers in order to get logged in
+// This is also where Google sends back information to our app once a user authenticates with Google
+app.get('/auth/google/callback',
+	passport.authenticate('google', { failureRedirect: '/', session: true }),
+	(req, res) => {
+		if(TEST) 
+			console.log('we authenticated, here is our user object:', req.user);
+			
+		res.redirect('/index');
+	}
+);
+
+app.get('/logout', function(req, res) {
+	req.logout();
+	res.redirect('/');
+});
+
+app.get('/', (req, res) => {
+	res.render('home.ejs', {auth: req.isAuthenticated()});
+});
+
+app.post('/dashboard', accessProtectionMiddleware, function (req, res) {
+	var latitude = req.body.latitude;
+	var longitude = req.body.longitude;
+	var umail = req.user.emails[0].value;
+
+	createCampo(umail, "campo1", latitude, longitude);
+	createSensore(umail, "campo1", "ID123883", "nomesensore");
+	createInnaffiamento(umail, "campo1", "ID123883", "1.3", "2019-02-20");
+		
+
+	var url = OWM_URL_1 + 'lat=' + latitude
+		+ '&lon=' + longitude + '&exclude=' + 'minutely,hourly,current' + OWM_URL_2
+
+	request(url, function (error, response, body) {
+		if (!error && response.statusCode == HTTP_OK) {
+
+			var info = JSON.parse(body).daily[0];
+			var main = info.temp;
+			var weather = info.weather[0].description;
+			var map_url = LL_URL_1 + longitude + ',' + latitude + LL_URL_2;
+
+			res.render('dashboard.ejs', {
+				lat: latitude,
+				lon: longitude,
+				weather: weather,
+				temp: KelvinToCelcius(main.day),
+				feels_like: KelvinToCelcius(info.feels_like.day),
+				min: KelvinToCelcius(main.min),
+				max: KelvinToCelcius(main.max),
+				pressure: info.pressure,
+				humidity: info.humidity,
+				port: PORT
+			});
+
+		}
+	});
+});
+
+// Avvio del server
+var server = app.listen(PORT, function () {
+	if(TEST) console.log("[!] Output will be verbose, test mode on!");
+	console.log('[i] Agrismart su http://localhost:%s\n', PORT);
+	// console.log(process.env.GOOGLE_OAUTH_TEST_APP_CLIENT_ID + "\n");
 });
