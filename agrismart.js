0ccd36defee4f3f22474d016adc863a53bf0d212
enzorevo <49289823+enzorevo@users.noreply.github.com>
2020-05-29 19:59:03 +0200

    finite primitive per il server

--- agrismart.js
+++ agrismart.js
@@ -110,47 +110,90 @@ function KelvinToCelcius(k) {
 /*         Database Firebase         */
 /*************************************/
 
+
+/*************************************/
+/*         Create User Firebase  	 */
+/*************************************/
+
 function createUser(email) { //creo l'utente
 	let instance = db.collection("users").doc(email);
 	instance.create({}).then(function() {
 		if(TEST) console.log("Utente aggiunto al database: "+ email);
+		// campicounter serve per tenere traccia dell'indice da dare al campo
+		instance.set({
+			campicounter: 0
+		}).then(function() {
+			if(TEST) console.log("[+] campicounter set to 0");
+		}).catch(function() {
+			if(TEST) console.log("error campicounter set");
+		});
 	}).catch((err) => {
 		if(TEST) console.log("Utente già registrato: "+ email);
 	});
 
-	// campicounter serve per tenere traccia dell'indice da dare al campo
-	instance.set({
-		campicounter: 0
-	}).then(function() {
-		if(TEST) console.log("[+] campicounter set to 0");
-	})
+	
 }
 
-function createCampo(email, name, lat, lon) { //creo il campo per l'utente
-	db.collection("users").doc(email).collection("campi").doc(name).set({
-		lat: lat,
-		lon: lon,
-	}).then(function() {
-		if(TEST) console.log("Campo "+ name +" aggiunto al database dell'utente: "+ email);
+/*************************************/
+/*         CreateCampo Firebase         */
+/*************************************/
+
+function createCampo(email, lat, lon) { //creo il campo per l'utente e ritorna l'id
+
+	db.collection("users").doc(email).get().then((userInstance) => {
+		var cid = parseInt(userInstance.data().campicounter) + 1;
+		var nomeCampo = "campo" + cid.toString();
+		if(TEST) console.log("Provo a creare" + nomeCampo);
+
+		db.collection("users").doc(email).update({
+			"campicounter": cid
+		}).then(() => {
+			if(TEST) console.log("[+] campicounter aggiornato");
+			db.collection("users").doc(email).collection("campi").doc(nomeCampo).set({
+				lat: lat,
+				lon: lon,
+			}).then(function() {
+				if(TEST) console.log("Campo "+ nomeCampo +" aggiunto al database dell'utente: "+ email);
+				
+			}).catch((err) => {
+				if(TEST) console.log("Campo già registrato: "+ nomeCampo);
+			});
+		}).catch((error) => {
+			console.log(error);
+		});
 	});
+	
 }
+
+
+/*************************************/
+/*         Create Sensore         */
+/*************************************/
 
 function createSensore(email, campo, id, name) { //creo il sensore sia nella sua tabella, sia per il rispettivo utente
 	db.collection("users").doc(email).collection("campi").doc(campo).collection("sensori").doc(id).create({
 		name: name,
 	}).then(function() {
 		if(TEST) console.log("Sensore "+ id +" aggiunto al database dell'utente: "+ email);
+		db.collection("sensors").doc(id).create({
+			email: email,
+			campo: campo,
+		}).then(function() {
+			if(TEST) console.log("Sensore "+ id +" aggiunto al database dei sensori con riferimento a: "+ email);
+		}).catch(function() {
+			if(TEST) console.log("Sensore "+ id +" non aggiunto al database dell'utente: "+ email);
+		});
+	}).catch(function() {
+		if(TEST) console.log("Sensore "+ id +" non aggiunto al database dell'utente: "+ email);
 	});
-
-	db.collection("sensors").doc(id).create({
-		email: email,
-		campo: campo,
-	}).then(function() {
-		if(TEST) console.log("Sensore "+ id +" aggiunto al database dei sensori con riferimento a: "+ email);
-	});
+	
 }
 
-function createInnaffiamento(email, campo, sensore, umidita, data) { //creo innaffiamento
+/*************************************/
+/*         Create Rilevazione         */
+/*************************************/
+
+function createRilevazione(email, campo, sensore, umidita, data) { //creo innaffiamento
 	db.collection("users").doc(email).collection("campi").doc(campo).collection("sensori").doc(sensore).collection("innaffiamenti").add({
 		umidita: umidita,
 		data: data,
@@ -163,65 +206,6 @@ function createInnaffiamento(email, campo, sensore, umidita, data) { //creo inna
 //vedere sul database quali sono, nel nostro caso email e campo, quindi ci si accede (ad esempio email con .email)
 //Gestire poi errori nel caso il documento non esista o ci sia un errore (in caso negativo restituisce undefined)
 
-
-/*function getSensorefromIdAux(id){
-	let documento= db.collection("sensors").doc(id);
-	return documento.get();
-}
-
-function promise2(doc){
-	if (!doc.exists) {
-		console.log('No such document!');
-		const result =  new Promise((resolve)=>{
-			resolve("errore");
-		});
-		return result;
-	} else {
-		console.log('Document data:', doc.data());
-			const result =  new Promise((resolve)=>{
-				resolve(doc.data());
-			});
-			return result;
-		}
-	}
-
-	function getSensorefromId(id){
-	return getSensorefromIdAux(id)
-	.then(promise2)
-	.catch((err) => console.log(err));
-}
-
-function getUltimiInnaffiamentiSensore(id){
-	getSensorefromId(id)
-	.then((x) => {
-		if (x != "errore"){
-			//Qui devo cercare l'utente
-			let documento = db.collection("users").doc(x.email).collection("campi").doc(x.campo).collection("sensori").doc(id).collection("innaffiamenti").get()
-			.then(function(snapshot) {
-				snapshot.forEach(function(userSnapshot) {
-				  console.log(userSnapshot.data().data);
-				});
-			});
-		}
-	});
-}
-
-function getnomesensorefromId(id){
-	getSensorefromId(id)
-	.then((x) => {
-		if (x != "errore"){
-			//Qui devo cercare l'utente
-			let documento = db.collection("users").doc(x.email).collection("campi").doc(x.campo).collection("sensori").doc(id).get()
-			.then( (x) => {
-			console.log(x.data().name);
-			return x.data().name;
-		});		
-	}
-		else{
-			return "Errore";
-		}
-	})
-}*/
 
 /************************************/
 /*         DB - getSensore          */
@@ -241,21 +225,25 @@ function getSensoreFromId(id, callback){
 
 
 
-function getInnaffiamentiFromDocumento(data, id, callback){
+function getRilevazioniFromDocumento(data, id, callback){
 	var keys = Array();
 	db.collection("users").doc(data.email).collection("campi").doc(data.campo).collection("sensori").doc(id).collection("innaffiamenti").get().then(function(snapshot) {
 		snapshot.forEach(function(userSnapshot) {
-			keys.push(userSnapshot.data().data);
+			keys.push([userSnapshot.data().data, userSnapshot.data().umidita]);
 		});
 		callback(null, keys);
 	});
 }
 
-function getInnaffiamentiFromSensorID(id) {
+/****************************************/
+/*    DB - getRilevazionifromSensordID  */
+/****************************************/
+
+function getRilevazioniFromSensorID(id) {
 	return new Promise(function(resolve, reject) {
 		async.waterfall([
 			async.apply(getSensoreFromId, id),
-			getInnaffiamentiFromDocumento,
+			getRilevazioniFromDocumento,
 		], (err, result) => {
 			if(err != null) reject(err);
 			else resolve(result);
@@ -280,6 +268,10 @@ function getAllSensori(){
 	});
 }
 
+/****************************************/
+/*         DB - getCampiFromUtente      */
+/****************************************/
+
 function getCampiFromUtente(email){
 	return new Promise(function(resolve,reject){
 		var keys = Array();
@@ -289,6 +281,23 @@ function getCampiFromUtente(email){
 			});
 			resolve(keys);
 		}).catch((y) => reject(y));
+	});
+}
+
+/****************************************/
+/*         DB - getSensoriFromUtente    */
+/****************************************/
+
+
+function getSensoriFromCampoUtente(email, campo) {
+	return new Promise(function(resolve,reject){
+		var keys = Array();
+		db.collection("users").doc(email).collection("campi").doc(campo).collection("sensori").get().then(function(snapshot) {
+			snapshot.forEach(function(userSnapshot) {
+				keys.push(userSnapshot.id);	
+			});
+			resolve(keys);
+		}).catch((err) => reject(err));
 	});
 }
 
@@ -304,16 +313,12 @@ function getArrayCampiFromUtente(email, callback){
 	});
 }
 
-async function getSensorifromArray(x, email, callback){
-	var keys = Array();
-	await x.forEach(async function (campo) {
-		db.collection("users").doc(email).collection("campi").doc(campo).collection("sensori").get().then(function(snapshot) {
-			snapshot.forEach(function(userSnapshot) {
-				keys.push(userSnapshot.id);
-			});
-		})
-	})
-	callback(null, keys);
+function getSensorifromArray(x, email, callback){
+	Promise.all(x.map((val) => {return getSensoriFromCampoUtente(email, val);})).then(values => { 
+		if(TEST) console.log(values.flat());
+		callback(null, values.flat());
+	}).catch((err) => callback("ERRORE"));
+	
 }
 
 function getSensorifromUtente(email) {
@@ -323,20 +328,27 @@ function getSensorifromUtente(email) {
 			getSensorifromArray,
 		], (err, result) => {
 			if(err != null) reject(err);
-			else {
-				console.log("finsco promise");
-				resolve(result);
-			}
+			else resolve(result);
 		});
 	});
 }
 
+/****************************************/
+/*         DB - getCampiCounter         */
+/****************************************/
+
+function getCampiCounter(email) {
+	return new Promise(function(resolve, reject) {
+		db.collection("users").doc(email).get().then(function(x) {
+			resolve(x.data().campicounter)
+		}).catch((y) => reject(y));
+	});
+}
+
+
 /***************************/
 /*         Routing         */
 /***************************/
-app.get('/prova', (req, res) => {
-	getSensorifromUtente("ermannino@gmail.com").then( (x) => res.send(x)).catch((y) => res.send("Errore"));
-});
 
 app.get('/index', accessProtectionMiddleware, function (req, res) {
 	var umail = req.user.emails[0].value;
@@ -375,7 +387,10 @@ app.get('/', (req, res) => {
 });
 
 app.get('/dashboard', function(req, res) {
-
+	//var umail = "ermannino@gmail.com";
+	//createCampo(umail, 41.33, 12.33);
+	//createSensore(umail, "campo102402841212", "ID12", "nomesensorenuovo");
+	//createRilevazione(umail, "campo102402841212", "ID12", "1.3", "2019-02-20");
 })
 
 app.post('/dashboard', accessProtectionMiddleware, function (req, res) {
@@ -390,53 +405,39 @@ app.post('/dashboard', accessProtectionMiddleware, function (req, res) {
 	userInstance invece è l'oggetto preso dal db e da lui posso
 	accedere ai dati
 	*/
-	var currentUser = db.collection("users").doc(umail)
+	
+	createCampo(umail, latitude, longitude);
+	createSensore(umail, "campo1", "ID123883", "nomesensore");
+	createRilevazione(umail, "campo1", "ID123883", "1.3", "2019-02-20");
+	
+	
+	var url = OWM_URL_1 + 'lat=' + latitude
+	+ '&lon=' + longitude + '&exclude=' + 'minutely,hourly,current' + OWM_URL_2
+	
+	request(url, function (error, response, body) {
+		if (!error && response.statusCode == HTTP_OK) {
+			
+			var info = JSON.parse(body).daily[0];
+			var main = info.temp;
+			var weather = info.weather[0].description;
+			// var map_url = LL_URL_1 + longitude + ',' + latitude + LL_URL_2;
+			
+			res.render('dashboard.ejs', {
+				lat: latitude,
+				lon: longitude,
+				weather: weather,
+				temp: KelvinToCelcius(main.day),
+				feels_like: KelvinToCelcius(info.feels_like.day),
+				min: KelvinToCelcius(main.min),
+				max: KelvinToCelcius(main.max),
+				pressure: info.pressure,
+				humidity: info.humidity,
+				port: PORT
+			});
+			
+		}
+	});
 
-	currentUser.get().then((userInstance) => {
-		var cid = userInstance.data().campicounter + 1;
-		var nomeCampo = "campo" + cid.toString();
-		console.log(nomeCampo);
-
-		currentUser.update({
-			"campicounter": cid
-		}).then(() => {
-			if(TEST) console.log("[+] campicounter aggiornato");
-		}).catch((error) => {
-			console.log(error);
-		});
-		console.log("sono quie")
-		createCampo(umail, nomeCampo, latitude, longitude);
-		createSensore(umail, nomeCampo, "ID123883", "nomesensore");
-		createInnaffiamento(umail, nomeCampo, "ID123883", "1.3", "2019-02-20");
-		
-		
-		var url = OWM_URL_1 + 'lat=' + latitude
-		+ '&lon=' + longitude + '&exclude=' + 'minutely,hourly,current' + OWM_URL_2
-		
-		request(url, function (error, response, body) {
-			if (!error && response.statusCode == HTTP_OK) {
-				
-				var info = JSON.parse(body).daily[0];
-				var main = info.temp;
-				var weather = info.weather[0].description;
-				// var map_url = LL_URL_1 + longitude + ',' + latitude + LL_URL_2;
-				
-				res.render('dashboard.ejs', {
-					lat: latitude,
-					lon: longitude,
-					weather: weather,
-					temp: KelvinToCelcius(main.day),
-					feels_like: KelvinToCelcius(info.feels_like.day),
-					min: KelvinToCelcius(main.min),
-					max: KelvinToCelcius(main.max),
-					pressure: info.pressure,
-					humidity: info.humidity,
-					port: PORT
-				});
-				
-			}
-		});
-	}).catch(function() {});
 });
 
 // Avvio del server
